17b3f6c992ae8e46e096287f35b2e39e9819ad40
KhaL <khale010698@gmail.com>
2025-07-02 09:22:30 +0700

    feat: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,24 @@
 #!/bin/bash
 
-# Stop bash when failer occur
-set -e
+# Start
+printf ">>>>> Starting \n"
+START_TIME=$(date +%s)
 
 # Constant
-PM2_INSTANCE_NAME="tonthepduybao-admin"
-PORT=7900
 
-read -p "Build branch: " branch
-read -p "Build env: " env
+BUILD_DIR=dist
+TARGET_DIR=/var/www/html/tonthepduybao.com/
 
+printf "\n>>>>> Fetching\n"
+git fetch && git pull
 
-# Script
-START_TIME=$(date +%s)
-echo "\n+++ Start +++"
+printf "\n\n>>>>> Building\n"
+yarn build
+sudo rm -rf $TARGET_DIR/*
+sudo cp -Rvp $BUILD_DIR/* $TARGET_DIR
+sudo rm -rf $BUILD_DIR
 
-echo "\n-- Switch to ${branch}"
-git checkout $branch
-
-echo "\n-- Pulling from ${branch}..."
-git pull
-
-echo "\n-- Installing..."
-rm -rf node_modules
-yarn install
-cp env.properties .env
-
-echo "\n-- Building at [${env}] environment..."
-NODE_ENV=$env yarn build
-pm2 delete $PM2_INSTANCE_NAME
-pm2 serve --spa dist/ --port $PORT --name $PM2_INSTANCE_NAME
-
+# End"
 END_TIME=$(date +%s)
 EXEC_TIME=$(($END_TIME-$START_TIME))
-echo "\n\n+++ Finish at $EXEC_TIME seconds +++\n"
+printf "\n\n>>>>> Finished at $EXEC_TIME secs\n"
